e02fcc8be5bb62400f030ecba61faa1bd1aca2fd
mirlanchik1337 <sadambekovmirlan9@gmail.com>
2023-04-27 21:39:22 +0500

    hw2

---
+++ product/models.py
@@ -0,0 +1,29 @@
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField(null=True, blank=True)
+    stars = models.IntegerField(blank=True, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_reviews')
+
+    def __str__(self):
+        return self.text
---
+++ product/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from product.models import Product, Review, Category
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = 'id title description price category'.split()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = Review
+        fields = 'product text stars'.split()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
